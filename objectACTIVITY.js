65ab22916f3957ef10ce8f7750d17a3b8dddd4c4
ck198 <charmevillanueva1@gmail.com>
2025-04-26 13:41:34 +0800

    Create objectACTIVITY.js

---
+++ objectACTIVITY.js
@@ -0,0 +1,17 @@
+// Create an object called car with the properties type, model, and color.
+const car = {
+    type: "Supercar",
+    model: "Lamborghini Aventador",
+    color: "Yellow"
+  };
+
+// Use typeof to check the type of the object and log it to the console.
+console.log (typeof car);
+
+// Update the type property to "Toyota" and log the updated object.
+car.type = "Toyota"
+console.log ("Updated object:", car);
+
+// Add a new property wheels with the value 4 and log the updated object.
+car.wheels = 4
+console.log (car)
